238fba5d122ebfbbcf3b83c5bd1c47820beee9f1
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-12-13 15:45:31 +0100

    add clock script

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-$(() => {
-	//jQuery code
-	console.info("jQuery has been loaded !");
-});
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+setDate = () => {
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondDegrees = ((seconds / 60) * 360); // Every second = 1 degree // Add a +offset if necessary
+	secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees = ((mins / 60) * 360);
+	minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = ((hours / 60) * 360);
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000); // Every seconds 
